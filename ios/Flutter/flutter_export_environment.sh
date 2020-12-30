f26818dd95f48db7b89d77d1a66b0f54faa15029
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2020-12-30 17:52:11 +0545

    Ignoring files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\gmaga\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Sweep\sweep_jonas\Sweep"
+export "FLUTTER_APPLICATION_PATH=D:\Sweep\sweep_master\Sweep"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
